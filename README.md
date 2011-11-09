6a217a7be3035c12a09e5f387f56778e293a4ba8
cenan ozen <cenan.ozen@gmail.com>
2011-11-09 23:43:03 +0200

    added chop-720-320-jpg.py

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-###py-image-tools
+#py-image-tools
 
-Python scripts for dealing with image files in batch
-
+Python scripts for dealing with image files in batch. The names of the scripts are self-explanatory.
---
+++ chop-720-320-jpg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import glob, os
+from PIL import Image
+
+def main():
+	for infile in glob.glob("*.jpg"):
+		file, ext = os.path.splitext(infile)
+		im = Image.open(infile)
+		im.crop((0,0,240,320)).save(file + ".1.jpg", "JPEG")
+		im.crop((240,0,480,320)).save(file + ".2.jpg", "JPEG")
+		im.crop((480,0,720,320)).save(file + ".3.jpg", "JPEG")
+
+if __name__=="__main__":
+	main()
+
