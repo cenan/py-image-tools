40d82677c19b441f2cab043e622898301e1d31ef
cenan ozen <cenan.ozen@gmail.com>
2011-12-25 20:08:51 +0200

    wallpaper

--- work-wallpaper.py
+++ work-wallpaper.py
@@ -6,10 +6,10 @@ from PIL import Image, ImageDraw, ImageFont
 WALLPAPER = '/home/cenan/Pictures/wallpaper.png'
 
 def main():
-    im = Image.new('RGB', (1920, 1080), (32, 32, 32, 255))
+    im = Image.new('RGBA', (1920, 1080), (64, 64, 64, 255))
     draw = ImageDraw.Draw(im)
-    font = ImageFont.truetype("/home/cenan/HelveticaNeueLight.ttf", 196)
-    draw.text((700, 400), "work!", (255, 255, 255), font=font)
+    font = ImageFont.truetype("/home/cenan/HelveticaNeueLight.ttf", 256)
+    draw.text((360, 360), "go to sleep", (255, 255, 255), font=font)
     im.save(WALLPAPER)
     if open("/etc/issue").read().startswith("Linux Mint 11 LXDE"):
         os.system('pcmanfm --set-wallpaper "%s"' % WALLPAPER)
